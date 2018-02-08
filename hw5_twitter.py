ccdb54dbec29958290a636fa3acc1a45f1a037a8
Rob Dalka <rpdalka@umich.edu>
2018-02-08 08:51:12 -0500

    Finished Part 3

--- hw5_twitter.py
+++ hw5_twitter.py
@@ -30,18 +30,44 @@ requests.get(url, auth=auth)
 #Code for Part 3:Caching
 #Finish parts 1 and 2 and then come back to this
 
+CACHE_FNAME = "tweet.json"
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    CACHE_DICTION = json.loads(cache_contents)
+    cache_file.close()
+except:
+    CACHE_DICTION = {}
+
+def get_request(baseurl, params):
+    unique_id = baseurl + "?screen_name=" + username + "&count=" + num_tweets
+    if unique_id in CACHE_DICTION:
+        print("Fectching data from chache...")
+        return CACHE_DICTION[unique_id]
+    else:
+        print("Making request for new data...")
+        json_file = requests.get(unique_id, auth = auth).text
+        CACHE_DICTION[unique_id] = json.loads(json_file)
+        dumped_json_cache = json.dumps(CACHE_DICTION)
+        fw = open(CACHE_FNAME,"w")
+        fw.write(dumped_json_cache)
+        fw.close()
+        return CACHE_DICTION[unique_id]
+
 #Code for Part 1:Get Tweets
+print('USER:', username)
+print('TWEETS ANALYZED:', num_tweets)
 base_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
 params = {'screen_name':username, 'count':num_tweets}
-json_file = requests.get(base_url, params, auth = auth).text
 # not necessary for rest of hw ##############
+#json_file = requests.get(base_url, params, auth = auth).text
 #with open('tweet.json', 'w') as outfile:
 #    json.dump(json_file, outfile, indent=4)
 #############################################
 
 #Code for Part 2:Analyze Tweets
 all_tweet_text = ""
-json_data = json.loads(json_file)
+json_data = get_request(base_url, params)
 for json_item in json_data:
     all_tweet_text += (" " + json_item['text'])
 
@@ -52,9 +78,10 @@ freqDist = nltk.FreqDist(token for token in tokens if token.isalpha()
                             and "https" not in token
                             and "RT" not in token)
 
+five_top = "5 MOST FREQUENT WORDS:"
 for word, frequency in freqDist.most_common(5):
-    print(word + " " + str(frequency))
-
+    five_top += (" " + word + " (" + str(frequency) + ")")
+print(five_top)
 
 
 if __name__ == "__main__":
